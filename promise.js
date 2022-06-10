3b0e4149f14c4bc201f9d3789cdf0a8f8ff6754c
Restiawan Yoga Pratama <restiawan008@gmail.com>
2022-06-10 15:45:38 +0700

    tantangan promise, commit pertama

--- promise.js
+++ promise.js
@@ -1,7 +1,21 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(emosi) => {
+  try {
+    let penontonIXX = await promiseTheaterIXX()
+    let penontonVGC = await promiseTheaterVGC()
+
+    let totalEmosiPenonton = [...penontonIXX, ...penontonVGC]
+
+    // jumlah penonton marah dan tidak marah
+    return totalEmosiPenonton.filter((totalEmosi)=> totalEmosi.hasil === emosi).length
+  } 
+  catch (err) {
+    console.log(err)
+  }
+
+};
 
 module.exports = {
   promiseOutput,
